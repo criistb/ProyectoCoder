2e18930283ae8618c7828908ed088b4080413fd9
criistb <103071014+criistb@users.noreply.github.com>
2023-01-15 14:59:02 -0500

    Carga de datos

---
+++ AppCoder/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+#Regier your models here
+
+
+admin.site.register(Curso)
+
+admin.site.register(Estudiante)
+
+admin.site.register(Profesor)
+
+admin.site.register(Entregable)
